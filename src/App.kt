3eff9fec36ea19611e026b7a98b43d16c00b7980
GABRIEL DA SILVA SOARES <24122283@dsn127m1246733.cfp127aluno.local>
2025-01-28 17:15:42 -0300

    primeiro cod

---
+++ src/App.kt
@@ -0,0 +1,37 @@
+import java.time.LocalDate
+import java.util.Scanner
+
+fun main() {
+
+    var leitor = Scanner(System.`in`)
+
+    var paciente1 = Paciente()
+    var paciente2 = Paciente()
+    var paciente3 = Paciente()
+
+    paciente1.nome = "Gabriel"
+    paciente1.dataNascimento = LocalDate.of(2008, 2, 15)
+    paciente1.peso = 69
+    paciente1.altura = 1.78f
+    paciente1.telefone = "(11)13131-3131"
+    paciente1.mostrarDados()
+
+    paciente2.nome = "Giovanna"
+    paciente2.dataNascimento = LocalDate.of(2007, 8, 31)
+    paciente2.peso = 54
+    paciente2.altura = 1.67f
+    paciente2.telefone = "(11)77777-7777"
+    paciente2.mostrarDados()
+
+    print("Qual o Nome do Paciente: ")
+    paciente3.nome = leitor.nextLine()
+    print("Qual a Data de Nascimento do Paciente:   ")
+    paciente3.dataNascimento = LocalDate.of(2007, 8, 31)
+    print("Qual o Peso do Paciente: ")
+    paciente3.peso = leitor.nextInt()
+    print("Qual a Altura do Paciente: ")
+    paciente3.altura = leitor.nextFloat()
+    print("Qual o Telefone do Paciente: ")
+    paciente3.telefone = leitor.nextLine()
+    paciente3.mostrarDados()
+}
--- src/Paciente.kt
+++ src/Paciente.kt
@@ -1,2 +1,55 @@
+import java.time.LocalDate
+import java.time.Period
+import kotlin.math.pow
+
 class Paciente {
+
+    var nome: String = ""
+    // ? => nullable safe ( força o valor ser nulo );
+    var dataNascimento: LocalDate? = null
+    var peso: Int = 0
+    // Float => Menor precisão ( por um 'f' no final para indiar um float); & Double => Maior precisão;
+    var altura: Float = 0.0f
+    var telefone: String = ""
+
+    fun calcularIMC(): Float{
+        var imc = peso / altura.pow(2)
+        return imc
+    }
+
+    fun mostrarDados(){
+        println("--------------------------")
+        println("DADOS DO PACIENTE")
+        println("--------------------------")
+        println("NOME: $nome")
+        println("IDADE: ${calcularIdade()}")
+        println("PESO: $peso")
+        println("ALTURA: $altura")
+        println("IMC: ${calcularIMC()}")
+        println("CLASSIFICAÇÃO: ${classificarIMC()}")
+        println("--------------------------")
+    }
+
+    fun classificarIMC(): String{
+        val imc = calcularIMC()
+        var classificacao = ""
+
+        if (imc <= 18.5){
+            classificacao = "Abaixo do Peso"
+        }else if (imc > 18.5 && imc <= 25.00){
+            classificacao = "Peso Ideal"
+        }else{
+            classificacao = "Acima do Peso"
+        }
+
+       return classificacao
+
+    }
+
+    fun calcularIdade(): Int{
+        val hoje = LocalDate.now()
+        val idade = Period.between(dataNascimento, hoje).years
+
+        return idade
+    }
 }
